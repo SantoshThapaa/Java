2bceb793f5c284d4897f6d34f5f79d2150bc137c
Santosh Thapa <santoshthapa@Santoshs-MacBook-Air.local>
2024-08-07 21:06:29 +0545

    First commit

---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = new int[5];
+
+        // Assign values to the array
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        // Print the values of the array
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
