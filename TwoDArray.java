aaa183d17957185d364e54d785a89ab8d79150df
Santosh Thapa <santoshthapa@Santoshs-MacBook-Air.local>
2024-08-09 14:26:52 +0545

    Second commit

---
+++ TwoDArray.java
@@ -0,0 +1,23 @@
+public class TwoDArray {
+    public static void main(String[] args) {
+        
+        int rows = 3;
+        int columns = 4;
+        int[][] array = new int[rows][columns];
+
+        int number = 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[i][j] = number++;
+            }
+        }
+
+        System.out.println("The 2D array elements are:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
