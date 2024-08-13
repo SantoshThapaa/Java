8764a71ebf54c65c5103bfc0e695475fa9fd0099
Santosh Thapa <santoshthapa@Santoshs-MacBook-Air.local>
2024-08-13 22:47:44 +0545

    Fourth commit

---
+++ FindAreas.java
@@ -0,0 +1,45 @@
+// Using run-time polymorphism.
+abstract class Figure { // Change 1: Make Figure an abstract class
+    double dim1;
+    double dim2;
+    Figure(double a, double b) {
+        dim1 = a;
+        dim2 = b;
+    }
+    abstract double area(); // Change 2: Make the area method abstract
+}
+
+class Rectangle extends Figure {
+    Rectangle(double a, double b) {
+        super(a, b);
+    }
+    // override area for rectangle
+    double area() {
+        System.out.println("Inside Area for Rectangle.");
+        return dim1 * dim2;
+    }
+}
+
+class Triangle extends Figure {
+    Triangle(double a, double b) {
+        super(a, b);
+    }
+    // override area for right triangle
+    double area() {
+        System.out.println("Inside Area for Triangle.");
+        return dim1 * dim2 / 2;
+    }
+}
+
+class FindAreas {
+    public static void main(String[] args) {
+        // Figure f = new Figure(10, 10); // This line can stay commented out or removed if necessary
+        Rectangle r = new Rectangle(9, 5);
+        Triangle t = new Triangle(10, 8);
+        Figure figref;
+        figref = r;
+        System.out.println("Area is " + figref.area());
+        figref = t;
+        System.out.println("Area is " + figref.area());
+    }
+}
